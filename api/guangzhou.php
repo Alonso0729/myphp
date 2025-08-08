c2a35a3e230f7169516150d0911c5bb60d804488
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-08-08 16:31:44 +0800

    Update on 20250808 part 5

---
+++ api/guangzhou.php
@@ -0,0 +1,10 @@
+<?php
+$data = json_decode(file_get_contents("https://gzbn.gztv.com:7443/plus-cloud-manage-app/liveChannel/queryLiveChannelList?type=1"))->data;//id=31-36
+$count = count($data);
+for($i=0;$i<$count;$i++){
+if($data[$i]->stationNumber == $_GET["id"]){
+$playurl = $data[$i]->httpUrl;
+break;
+}}
+header("Location: {$playurl}",true,302);
+?>
