b8902dd15782433327de783ce6e2cd1af5b7f85b
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-08-13 14:47:50 +0800

    Update on 20250813 part 5

---
+++ api/mgzb.php
@@ -0,0 +1,191 @@
+<?php
+error_reporting(0);
+$n = [
+    'CCTV1' => '608807420', //CCTV1综合
+    'CCTV2' => '631780532', //CCTV2财经
+    'CCTV3' => '624878271', //CCTV3综艺
+    'CCTV4' => '631780421', //CCTV4中文国际
+    'CCTV4a' => '608807416', //CCTV4美洲
+    'CCTV4o' => '608807419', //CCTV4欧洲
+    'CCTV5' => '641886683', //CCTV5体育
+    'CCTV5p' => '641886773', //CCTV5+体育赛事
+    'CCTV6' => '624878396', //CCTV6电影
+    'CCTV7' => '673168121', //CCTV7国防军事
+    'CCTV8' => '624878356', //CCTV8电视剧
+    'CCTV9' => '673168140', //CCTV9纪录
+    'CCTV10' => '624878405', //CCTV10科教
+    'CCTV11' => '667987558', //CCTV11戏曲
+    'CCTV12' => '673168185', //CCTV12社会与法
+    'CCTV13' => '608807423', //CCTV13新闻
+    'CCTV14' => '624878440', //CCTV14少儿
+    'CCTV15' => '673168223', //CCTV15音乐
+    'CCTV17' => '673168256', //CCTV17农业农村
+    'fxzl' => '624878970', //CCTV发现之旅
+    'lgs' => '884121956', //CCTV老故事
+    'cctvzxs' => '708869532', //CCTV中学生
+     'cctvnews' => '609017205', //CGTN
+     'cctvdoc' => '609006487', //CGTN纪录
+     'cgtne' => '609006450', //CCTV西班牙语
+     'cgtnf' => '609006476', //CCTV法语
+    'cgtna' => '609154345', //CCTV阿拉伯语
+    'cgtnr' => '609006446', //CCTV俄语
+   'chcdz' => '644368714', //CHC动作电影
+'chcjt' => '644368373', //CHC家庭影院
+'chc' => '952383261', //CHC影迷电影
+
+   'dfwshd' => '651632648', //东方卫视
+'cqws' => '738910914', //重庆卫视
+'jlws' => '947472500', //吉林卫视
+'qhws' => '947472506', //青海卫视
+'lnws' => '630291707', //辽宁卫视
+'nmws' => '738911430', //内蒙古卫视
+'nxws' => '738910535', //宁夏卫视
+'gsws' => '738910997', //甘肃卫视
+
+'sxws' => '738910838', //陕西卫视
+'sdws' => '738910366', //山东卫视
+'henwshd' => '790187291', //河南卫视
+'hubwshd' => '947472496', //湖北卫视
+'hunwshd' => '635491149', //湖南卫视
+'jxws' => '783847495', //江西卫视
+'jswshd' => '623899368', //江苏卫视
+'dnwshd' => '849116810', //东南卫视
+    'lvyou' => '947472502', //海南卫视
+    'hixws' => '849119120', //海峡卫视
+'gdwshd' => '608831231', //广东卫视
+'dwqws' => '608917627', //大湾区卫视
+'gxws' => '783844132', //广西卫视
+'ynws' => '783846580', //云南卫视
+'gzws' => '783845222', //贵州卫视
+'xjws' => '738910476', //新疆卫视
+'xzws' => '738910461', //西藏卫视
+'hinws' => '738906860', //海南卫视
+
+'shdy' => '895358641', //四海钓鱼
+
+'stvdfys' => '617290047', //东方影视
+'stvnewshd' => '651632657', //上海新闻综合
+'dycj' => '608780988', //上海第一财*
+'stvjishihd' => '617289997', //上视纪实人文
+'stvicshd' => '618954688', //上海外语
+'fztd' => '790188943', //法治天地
+'jbtyhd' => '796071336', //劲爆体育
+'mlzq' => '796070308', //魅力足球
+'ly' => '796070452', //乐游
+'hxjchd' => '790187880', //欢笑剧场
+'qcxj' => '796071456', //七彩戏剧
+'yxfy' => '790188417', //游戏风云
+'gdys' => '629943678', //广东影视
+'lttv' => '668225749', //临洮电视台
+
+'jscs' => '626064714', //江苏城市
+'jszy' => '626065193', //江苏综艺
+'jsys' => '626064697', //江苏影视
+'jsggxw' => '626064693', //江苏公共新闻
+'jsgj' => '626064674', //江苏国际
+'jsjy' => '628008321', //江苏教育
+'jstyxx' => '626064707', //江苏体育休闲
+'ymkt' => '626064703', //优漫卡通
+
+'njxwzh' => '838109047', //南京新闻综合
+'njkj' => '838153729', //南京教科
+'nj18' => '838151753', //南京十八
+
+'haxwzh' => '639731826', //淮安新闻综合
+'lygxwzh' => '639731715', //连云港新闻综合
+'szxwzh' => '639731952', //苏州新闻综合
+'tzxwzh' => '639731818', //泰州新闻综合
+'sqxwzh' => '639731832', //宿迁新闻综合
+'xzxwzh' => '639731747', //徐州新闻综合
+
+'gdys' => '614961829', //广东影视
+'jjkt' => '614952364', //嘉佳卡通
+
+'24hyzb' => '895932793', //24小时亚洲杯频道
+'cbajd' => '788813182', //CBA经典
+'tvbjc' => '631354620', //掼蛋精英赛
+'gqdp' => '629943678', //高清大片
+'hpjxss' => '780290695', //和平精英赛事
+'hsdzjc' => '713600957', //红色轮播台
+'jddhdjh' => '629942219', //经典动画大集合
+'jdxgdy' => '625703337', //经典深圳旁边电影
+'jsmdp' => '617432318', //军事迷必看大片
+'ljcwhg' => '707671890', //历届春晚回顾
+'mg24hty' => '654102378', //咪咕24小时体育台
+'mglbt' => '788816794', //玫瑰轮播台
+'nbajd' => '788815380', //NBA经典
+'ozzqfy' => '788816794', //欧洲足球风云
+'qmpp' => '788818045', //全民乒乓
+'rjlb' => '629943613', //热剧联播
+'sszjd' => '646596895', //赛事最经典
+'ttmlh' => '629943305', //体坛名栏汇
+'ufcgdjx' => '788818804', //UFC格斗精选
+'wdlsjd' => '780288994', //五大联赛经典
+'xczx' => '713589837', //乡村振兴
+'xfzgn' => '617432318', //幸福中国年
+'xpfyt' => '619495952', //新片放映厅
+'yxlmss' => '780286989', //英雄联盟赛事
+'zjlxc' => '625133682', //周杰伦现场
+'zqzyp' => '629942228', //最强综艺趴
+];
+$id = isset($_GET['ur']) ? $_GET['ur'] : 'CCTV1';
+$contId = $_GET['contId']??'';
+$playseek = $_GET['playseek']??'';
+if ($playseek) {
+  $playseekArray = explode('-',$playseek);
+$bstrURL = "https://webapi.miguvideo.com/gateway/playurl/v3/play/playurl?contId={$n[$id]}{$contId}&channelId=0132_10010001005"; }else{$bstrURL = "https://webapi.miguvideo.com/gateway/playurl/v3/play/playurl?contId={$n[$id]}&channelId=0132_10010001005";}
+$live = json_decode(get_data($bstrURL), 1)['body']['urlInfo']['url'];
+$uas = parse_url($live);
+parse_str($uas["query"], $arr);
+$puData = str_split($arr['puData']);
+$ProgramID = str_split($n[$id]);
+$Program = str_split('yzwxcdwbgh');
+$s = count($puData);
+$arr_key = [];
+for ($v = 0; $v < $s / 2; $v++) {
+    $arr_key[] = $puData[$s - $v - 1];
+    $arr_key[] = $puData[$v];
+    switch ($v) {
+        case 1:
+        case 2:
+        case 4:
+            $arr_key[] = arrkey($v);
+            break;
+        case 3:
+            $arr_key[] = $Program[$ProgramID[1]];
+            break;
+        }
+    }
+$ddCalcu = join($arr_key);
+
+$p = $live . "&ddCalcu=" . $ddCalcu . '&sv=10000&crossdomain=www&ct=h5';
+
+$live = get_data($p, $headers);
+//$host = parse_url($live)['host'];
+//$playurl = preg_replace("|{$host}:443|","hlstxmgsplive.miguvideo.com",$live);
+if ($playseek) {
+        $playseekArray = explode('-',$playseek);
+        $starttime = strtotime($playseekArray[0]);
+        $endtime = strtotime($playseekArray[1]);
+        $st= $playseekArray[0];
+        $et= $playseekArray[1];
+        $$live=''.$$live.'&playbackbegin='.$st.'&playbackend='.$et.'';
+}
+header('Location: ' . $live);
+//echo $$live;
+exit;
+
+function arrkey($v) {
+    $put = ['z', 'y', '0', 'z'];
+    return $put[$v - 1];
+    }
+function get_data($url) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+    }
+?>
