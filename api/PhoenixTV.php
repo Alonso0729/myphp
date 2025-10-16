b5ed40381b2fdd6820aae14b6a5eb5f61c3c7c91
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-10-16 16:02:55 +0800

    Update on 20251016 part 2

---
+++ api/PhoenixTV.php
@@ -0,0 +1,16 @@
+<?php
+// 获取频道ID参数
+$id = isset($_GET['id'])?$_GET['id']:'gzws';
+$n = [
+	"fhzw" => "cn",  // 凤凰中文
+    "fhzx" => "info", // 凤凰资讯
+    "fhhk" => "hk"   // 凤凰香港
+    ];
+	
+$token = "id=cn&prefix=86&phone=13256889895&pwd=Fan2345678";
+//备用号码1:phone=13389247903&pwd=Llxxcc198,备用号码2:phone=13955036885&pwd=make123456MAKE
+$streamUrl = "http://tv.groupshare.com.cn/fhtv?id=$n[$id]&token=${token}";
+
+header('Location: ' . $streamUrl);
+exit;
+?>
