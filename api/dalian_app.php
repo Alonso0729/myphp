7386c8e2e101e03f8e7585c048dc95979f07a0df
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-11-09 19:34:26 +0800

    Update on 20251109 part 3

--- api/dalian_app.php
+++ api/dalian_app.php
@@ -4,9 +4,9 @@ $idn = array(
 "dlxwzh" => "tcb3IB5",//大连新闻综合
 "dlsh" => "JzcFkF4",//大连生活
 "dlwt" => "hxT7Fc3",//大连文体
-"dlys" => "8cuL6wa",//大连影视
-"dlse" => "q6tZ6Ba",//大连少儿
-"dlgw" => "N4S4uAj",//大连购物
+//"dlys" => "8cuL6wa",//大连影视
+//"dlse" => "q6tZ6Ba",//大连少儿
+"dlgw" => "N4S4uAj",//大连乐天购物
 );
 $url="http://dlyapp.dltv.cn/apiv4.0/m3u8_notoken.php?channelid=".$idn[$id];
 $info = file_get_contents($url);
