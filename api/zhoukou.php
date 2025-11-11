0be541b284a8d8b67c5f5c50046621854a311341
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-11-11 14:48:50 +0800

    Update on 20251111 part 2

--- api/zhoukou.php
+++ api/zhoukou.php
@@ -4,7 +4,7 @@ $n = array(
    'zkxwzh' => 1,//周口新闻综合
    'zkgg' => 2,//周口公共频道
    'zkjy' => 3,//周口教育频道
-   'zkhc' => 4,//周口川汇频道
+   'zkch' => 4,//周口川汇频道
 );
 
 $d=file_get_contents('http://mms.yszkapp.cn:18080/mms4.6.3/videoPlayInterface/getChannelInfo.jspa?token=pmqhukcpyipvqmoz&channelId='.$n[$id]);
