a606d3736c7d4e48a5df0d71569b9b86bb99c8c5
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-11-16 13:53:30 +0800

    Update on 20251116

--- api/bst.php
+++ api/bst.php
@@ -12,8 +12,14 @@ function main($group, $type, $code, &$play_url)
     $response_msg = '';
     for ($i = 1; $i <= 10; $i++) {
         foreach (hosts_get() as $host) {
-            $u = intf_get($host, $group, $type, $code);
-            $c = @file_get_contents($u);
+            list($u, $p) = intf_get($host, $group, $type, $code);
+            $c = @file_get_contents($u, false, stream_context_create(array(
+                'http' => array(
+                    'method' => 'POST',
+//                    'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+                    'content' => $p,
+                )
+            )));
             if ($c === false)
                 continue;
             $play_url = url_get($type, $c, $response_code, $response_msg);
@@ -34,9 +40,11 @@ function main($group, $type, $code, &$play_url)
 
 function intf_get($host, $group, $type, $code)
 {
-    $f = 'http://%s/ps/OttService/Auth?UserID=%s&UserToken=%s&TVID=$$%s&UserGroup=$TerOut_%d&ItemType=%d&ItemCode=%s';
-    $r = sprintf($f, $host, user_get_id(), user_get_token(), tvid_get(), $group, $type, $code);
-    return $r;
+    $f = 'http://%s/ps/OttService/Auth';
+    $u = sprintf($f, $host);
+    $f = 'UserID=%s&UserToken=%s&TVID=$$%s&UserGroup=$TerOut_%d&ItemType=%d&ItemCode=%s';
+    $p = sprintf($f, user_get_id(), user_get_token(), tvid_get(), $group, $type, $code);
+    return [$u, $p];
 }
 
 //function type_get()
@@ -46,20 +54,20 @@ function intf_get($host, $group, $type, $code)
 
 function tvid_get()
 {
-    return random_strings(4, 16);
+    return random_string(4, 16);
 }
 
 function user_get_token()
 {
-    return random_strings(1, 16);
+    return random_string(1, 16);
 }
 
 function user_get_id()
 {
-    return random_strings(1, 16);
+    return random_string(1, 16);
 }
 
-function random_strings($min, $max)
+function random_string($min, $max)
 {
     $r = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0,
         mt_rand($min, $max));
@@ -113,14 +121,14 @@ function locate($url)
 央视,#genre#
 CCTV-1HD,bst.php?g=1&t=2&c=Umai:CHAN/111128@BESTV.SMG.SMG
 CCTV-2HD,bst.php?g=1&t=2&c=Umai:CHAN/5000036@BESTV.SMG.SMG
-CCTV-3HD,bst.php?g=14&t=2&c=Umai:CHAN/1369028@BESTV.SMG.SMG
+CCTV-3HD,bst.php?g=29&t=2&c=Umai:CHAN/1369028@BESTV.SMG.SMG
 CCTV-4,bst.php?g=1&t=2&c=Umai:CHAN/1349@BESTV.SMG.SMG
-CCTV-5HD,bst.php?g=14&t=2&c=Umai:CHAN/1369029@BESTV.SMG.SMG
-CCTV-5+HD,bst.php?g=14&t=2&c=Umai:CHAN/6000068@BESTV.SMG.SMG
-CCTV-6HD,bst.php?g=14&t=2&c=Umai:CHAN/1369030@BESTV.SMG.SMG
+CCTV-5HD,bst.php?g=29&t=2&c=Umai:CHAN/1369029@BESTV.SMG.SMG
+CCTV-5+HD,bst.php?g=29&t=2&c=Umai:CHAN/6000068@BESTV.SMG.SMG
+CCTV-6HD,bst.php?g=29&t=2&c=Umai:CHAN/1369030@BESTV.SMG.SMG
 CCTV-7,bst.php?g=1&t=2&c=Umai:CHAN/1352@BESTV.SMG.SMG
-CCTV-8HD,bst.php?g=14&t=2&c=Umai:CHAN/1369033@BESTV.SMG.SMG
-CCTV-9HD,bst.php?g=14&t=2&c=Umai:CHAN/5000039@BESTV.SMG.SMG
+CCTV-8HD,bst.php?g=29&t=2&c=Umai:CHAN/1369033@BESTV.SMG.SMG
+CCTV-9HD,bst.php?g=29&t=2&c=Umai:CHAN/5000039@BESTV.SMG.SMG
 CCTV-10,bst.php?g=1&t=2&c=Umai:CHAN/1355@BESTV.SMG.SMG
 CCTV-11,bst.php?g=1&t=2&c=Umai:CHAN/1356@BESTV.SMG.SMG
 CCTV-12,bst.php?g=1&t=2&c=Umai:CHAN/1357@BESTV.SMG.SMG
@@ -128,12 +136,12 @@ CCTV-13HD,bst.php?g=1&t=2&c=Umai:CHAN/6000054@BESTV.SMG.SMG
 CCTV-13,bst.php?g=1&t=2&c=Umai:CHAN/1358@BESTV.SMG.SMG
 CCTV-14,bst.php?g=1&t=2&c=Umai:CHAN/1359@BESTV.SMG.SMG
 CCTV-15,bst.php?g=1&t=2&c=Umai:CHAN/3874@BESTV.SMG.SMG
-CCTV-16HD,bst.php?g=14&t=2&c=Umai:CHAN/6000061@BESTV.SMG.SMG
-CCTV-17HD,bst.php?g=14&t=2&c=Umai:CHAN/5000041@BESTV.SMG.SMG
+CCTV-16HD,bst.php?g=29&t=2&c=Umai:CHAN/6000061@BESTV.SMG.SMG
+CCTV-17HD,bst.php?g=29&t=2&c=Umai:CHAN/5000041@BESTV.SMG.SMG
 
 卫视,#genre#
 安徽卫视HD,bst.php?g=1&t=2&c=Umai:CHAN/3540416@BESTV.SMG.SMG
-北京卫视HD,bst.php?g=14&t=2&c=Umai:CHAN/181361@BESTV.SMG.SMG
+北京卫视HD,bst.php?g=29&t=2&c=Umai:CHAN/181361@BESTV.SMG.SMG
 北京卫视,bst.php?g=1&t=2&c=Umai:CHAN/1326@BESTV.SMG.SMG
 广东深圳卫视HD,bst.php?g=1&t=2&c=Umai:CHAN/181362@BESTV.SMG.SMG
 黑龙江卫视,bst.php?g=1&t=2&c=Umai:CHAN/1343@BESTV.SMG.SMG
@@ -155,13 +163,13 @@ CCTV-17HD,bst.php?g=14&t=2&c=Umai:CHAN/5000041@BESTV.SMG.SMG
 东方购物-1,bst.php?g=1&t=2&c=Umai:CHAN/648549@BESTV.SMG.SMG
 都市剧场,bst.php?g=1&t=2&c=Umai:CHAN/1366@BESTV.SMG.SMG
 都市频道,bst.php?g=1&t=2&c=Umai:CHAN/1318@BESTV.SMG.SMG
-哈哈炫动HD,bst.php?g=14&t=2&c=Umai:CHAN/4641019@BESTV.SMG.SMG
+哈哈炫动HD,bst.php?g=29&t=2&c=Umai:CHAN/4641019@BESTV.SMG.SMG
 哈哈炫动,bst.php?g=1&t=2&c=Umai:CHAN/1324@BESTV.SMG.SMG
 欢笑剧场,bst.php?g=1&t=2&c=Umai:CHAN/1376@BESTV.SMG.SMG
-七彩戏剧HD,bst.php?g=14&t=2&c=Umai:CHAN/6880440@BESTV.SMG.SMG
+七彩戏剧HD,bst.php?g=29&t=2&c=Umai:CHAN/6880440@BESTV.SMG.SMG
 七彩戏剧,bst.php?g=1&t=2&c=Umai:CHAN/1374@BESTV.SMG.SMG
 外语频道HD,bst.php?g=1&t=2&c=Umai:CHAN/3778907@BESTV.SMG.SMG
-新闻综合HD,bst.php?g=14&t=2&c=Umai:CHAN/1346060@BESTV.SMG.SMG
+新闻综合HD,bst.php?g=29&t=2&c=Umai:CHAN/1346060@BESTV.SMG.SMG
 新闻综合,bst.php?g=1&t=2&c=Umai:CHAN/1312@BESTV.SMG.SMG
 游戏风云HD,bst.php?g=1&t=2&c=Umai:CHAN/1555894@BESTV.SMG.SMG
 
