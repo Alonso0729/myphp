404c9ec2485e944a961c1486e40e893a9b38c880
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-12-16 13:50:53 +0800

    Update on 20251216

--- api/shenzhen.php
+++ api/shenzhen.php
@@ -1,36 +1,72 @@
 <?php
+$id = isset($_GET['channel']) ? $_GET['channel'] : '';
+if ($id) {
+    $id  = strtolower($id);
 
-date_default_timezone_set('Asia/Shanghai');
-$id = $_GET['channel']?:"深圳卫视";
-$id  = strtolower($id);
+    $ids = [
+        '深圳卫视' => 'AxeFRth',
+        '深圳少儿' => '1SIQj6s',
+        '深圳财经' => '3vlcoxP',
+        '深圳电视剧' => '4azbkoY',
+        '宜和购物' => 'BJ5u5k2',
+        '深圳都市' => 'ZwxzUXr',
+        '深圳国际' => 'sztvgjpd',
+        '深圳移动' => 'wDF6KJ3',
+        '深圳卫视4k' => 'R77mK1v'
 
-$hosts = "https://sztv-hls.sztv.com.cn";
+    ];
+    $key = "bf9b2cab35a9c38857b82aabf99874aa96b9ffbb";
+    $dectime = dechex(time()+7200);
+    $rate = "500";
+    $path = '/'.$ids[$id].'/'.$rate.'/'.pathname($ids[$id]).'.m3u8';
 
-$ids = [
-    '深圳卫视' => 'AxeFRth',
-    '深圳少儿' => '1SIQj6s',
-    '深圳财经' => '3vlcoxP',
-    '深圳电视剧' => '4azbkoY',
-    '宜和购物' => 'BJ5u5k2',
-    '深圳都市' => 'ZwxzUXr',
-    '深圳国际' => 'sztvgjpd',
-    '深圳移动' => 'wDF6KJ3',
-    '深圳卫视4k' => 'R77mK1v'
+    $sign = md5($key.$path.$dectime);
+    $liveURL = $path."?sign={$sign}&t={$dectime}";
+    $liveURL = $_SERVER['PHP_SELF'] . '?m3u8=' . rawurlencode($liveURL);
 
-];
-$key = "bf9b2cab35a9c38857b82aabf99874aa96b9ffbb";
-$dectime = dechex(time()+7200);
-$rate = "500";
-$path = '/'.$ids[$id].'/'.$rate.'/'.pathname($ids[$id]).'.m3u8';
+    header('Access-Control-Allow-Origin: *');
+    header("Location: $liveURL");
+    die;
+}
 
-$sign = md5($key.$path.$dectime);
-$liveURL = $hosts.$path."?sign={$sign}&t={$dectime}";
+$m3u8 = isset($_GET['m3u8']) ? $_GET['m3u8'] : '';
+if ($m3u8) {
+    $c = get($m3u8);
+    $p = $_SERVER['PHP_SELF'] . '?ts=' . dirname($m3u8) . '/';
+    $c = preg_replace('/^(?=.+\.ts)/m', $p, $c);
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/vnd.apple.mpegurl');
+    echo $c;
+    die;
+}
 
-header("location: $liveURL");
+$ts = isset($_GET['ts']) ? $_GET['ts'] : '';
+if ($ts) {
+    $c = get($ts);
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: video/MP2T');
+    echo $c;
+    die;
+}
+
+
+
+function get($path) {
+    $url = 'https://sztv-hls.sztv.com.cn' . $path;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_REFERER,'https://www.sztv.com.cn/');
+//    curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.95 Safari/537.36');
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
 
 function pathname($e)
 {
-
+    date_default_timezone_set('Asia/Shanghai');
     $o = strtotime('today')*1000;
 
 
@@ -112,5 +148,3 @@ function pathname($e)
 深圳宜和购物频道,sz.php?channel=%E5%AE%9C%E5%92%8C%E8%B4%AD%E7%89%A9
 深圳国际频道,sz.php?channel=%E6%B7%B1%E5%9C%B3%E5%9B%BD%E9%99%85
 */
-
-?>
