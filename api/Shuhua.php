8e6101aa1487bb732efa3722c902dd33595d7c3a
Alonso0729 <Eason.Zhang0731@outlook.com>
2026-03-02 20:20:46 +0800

    Update on 20260302 part 2

--- api/Shuhua.php
+++
@@ -1,5 +0,0 @@
-<?php
-$html = file_get_contents('http://web.shtv.net.cn/MobileWeb/OnlineLive.aspx');
-preg_match('/src="(http[^"]+\.m3u8[^"]*)"/i', $html, $m);
-header('Location: ' . ($m[1] ?? 'about:blank'));
-?>
